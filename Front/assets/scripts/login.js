f1abbd0334b0cb5184816273bb3b643bd7493001
LucioNucera <nucera.lucio@gmail.com>
2025-01-15 12:23:07 +0100

    login-ruolo

--- Front/assets/scripts/login.js
+++ Front/assets/scripts/login.js
@@ -119,50 +119,55 @@
 // FINE SCRIPT DEL MODALE LOGIN - VA AGGIUNTA FUNZIONALITA' CRUD
 
 document.addEventListener('DOMContentLoaded', () => {
-    const loginButton = document.getElementById('loginButton'); 
-    const usernameInput = document.getElementById('inputEMail');
-    const passwordInput = document.getElementById('inputPassword');
-    
-    loginButton.addEventListener('click', async (e) => {
-      e.preventDefault(); 
+  const loginButton = document.getElementById('loginButton'); 
+  const usernameInput = document.getElementById('inputEMail');
+  const passwordInput = document.getElementById('inputPassword');
   
+  loginButton.addEventListener('click', async (e) => {
+      e.preventDefault(); 
+
       const username = usernameInput.value;
       const password = passwordInput.value;
-  
+
       if (username && password) {
-        const user = {
-          email: username,
-          password: password
-        };
-  
-        try {
-          const response = await fetch('http://127.0.0.1:8080/auth/login', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user),
-          });
-  
-          if (response.ok) {
-            
-            const loggedInUser = await response.json();
-  
-            
-            localStorage.setItem('currentUser', JSON.stringify(loggedInUser));
-  
-           
-            window.location.href = 'index.html';
-          } else {
-            alert('Credenziali non valide!');
+          const user = {
+              email: username,
+              password: password
+          };
+
+          try {
+              const response = await fetch('http://127.0.0.1:8080/auth/login', {
+                  method: 'POST',
+                  headers: {
+                      'Content-Type': 'application/json'
+                  },
+                  body: JSON.stringify(user),
+              });
+
+              if (response.ok) {
+                  const loggedInUser = await response.json();
+
+                
+                  localStorage.setItem('currentUser', JSON.stringify(loggedInUser));
+
+                  // RUOLO
+                  const role = loggedInUser.ruolo;
+                  if (role === 'ADMIN') {
+                      window.location.href = 'panel.html'; 
+                  } else if (role === 'UTENTE') {
+                      window.location.href = 'user.html'; 
+                  } else {
+                      alert('Ruolo non riconosciuto. Contatta l\'amministratore di sistema.');
+                  }
+              } else {
+                  alert('Credenziali non valide!');
+              }
+          } catch (error) {
+              console.error('Errore nella richiesta di login:', error);
+              alert('Si è verificato un errore durante il login. Riprova più tardi.');
           }
-        } catch (error) {
-          console.error('Errore nella richiesta di login:', error);
-          alert('Si è verificato un errore durante il login. Riprova più tardi.');
-        }
       } else {
-        alert('Per favore, inserisci sia lo username che la password.');
+          alert('Per favore, inserisci sia lo username che la password.');
       }
-    });
   });
-  
+});
